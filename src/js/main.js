0ab11158041db214fe97e2294d4a0ec50fb59463
katekosm <kat.kosmynina@mail.ru>
2022-04-29 09:17:09 +0300

    finished dynamic product list

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 import ProductData from './productData.js';
 import Alert from './alert.js';
+import ProductList from './productList.js';
 
 const dataSource = new ProductData('alerts');
 const alert = new Alert(dataSource);
 alert.init();
+
+const tents = new ProductData('tents');
+const listElement = document.querySelector('.product-list');
+const list = new list('tents', tents, listElement);
+
+list.init();
---
+++ src/js/productList.js
@@ -0,0 +1,47 @@
+import { renderListWithTemplate } from './utils.js';
+
+export default class ProductListing {
+    constructor(category, dataSource, listElement) {
+        // We passed in this information to make our class as reusable as possible.
+        // Being able to define these things when we use the class will make it very flexible
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+    async init() {
+        // our dataSource will return a Promise...so we can use await to resolve it.
+        let list = await this.dataSource.getData();
+        list = this.onlyFour(list);
+        this.renderList(list);
+    }
+    onlyFour(list) {
+        return list.filter((item, index) => index < 4);
+    }
+    prepareTemplate(template, product) {
+        template.querySelector('a').href += product.Id;
+        template.querySelector('img').src = product.Image;
+        template.querySelector('img').alt += product.Name;
+        template.qsuerySelector('.card__brand').textContent = product.Brand.Name;
+        template.querySelector('.card__name').textContent = product.NameWithoutBrand;
+        template.querySelector('.product-card__price').textContent += product.FinalPrice;
+        return template;
+    }
+    renderList(list) {
+            // make sure the list is empty
+            this.listElement.innerHTML = '';
+            //get the template
+            const template = document.getElementById('product-card-template');
+            renderListWithTemplate(template, this.listElement, list, this.prepareTemplate);
+
+        }
+        // original method before moving the template logic to utils.js
+        // renderList(list) {
+        // const template = document.getElementById('product-card-template');
+        // list.forEach(product => {
+        //   const clone = template.content.cloneNode(true);
+        //   const hydratedTemplate = this.prepareTemplate(clone, product);
+        //   this.listElement.appendChild(hydratedTemplate);
+        // })
+        // }
+
+}
