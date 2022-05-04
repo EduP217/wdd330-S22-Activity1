434cd4657460c9e94d6206e7d7406433500eb36a
katekosm <kat.kosmynina@mail.ru>
2022-05-04 12:53:21 +0300

    finished the individual activity for the third week

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,17 +1,17 @@
 function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
+    return JSON.parse(localStorage.getItem(key));
 }
 
 function getCartContents() {
-  // let markup = '';
-  const cartItems = getLocalStorage('so-cart');
-  const htmlItems = cartItems.map((item) => renderCartItem(item));
-  document.querySelector('.product-list').innerHTML = htmlItems.join('');
-  // document.querySelector('.product-list').innerHTML = renderCartItem(cartItems);
+    // let markup = '';
+    const cartItems = getLocalStorage('so-cart');
+    const htmlItems = cartItems.map((item) => renderCartItem(item));
+    document.querySelector('.product-list').innerHTML = htmlItems.join('');
+    // document.querySelector('.product-list').innerHTML = renderCartItem(cartItems);
 }
 
 function renderCartItem(item) {
-  const newItem = `<li class="cart-card divider">
+    const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -25,8 +25,16 @@ function renderCartItem(item) {
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
-  console.log(newItem);
-  return newItem;
+    console.log(newItem);
+    return newItem;
 }
 
 getCartContents();
+
+import { loadHeaderFooter } from './utils.js';
+import CartList from './cartList.js';
+
+loadHeaderFooter();
+
+const cart = new CartList('so-cart', document.querySelector('.product-list'));
+cart.init();
