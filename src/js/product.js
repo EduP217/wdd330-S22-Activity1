49888b69df00e9451e2f34ecbbe424abbfca147f
EduP217 <prieto_217@hotmail.com>
2022-05-06 08:35:28 -0500

    completing individual activity 4

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,10 @@
 import ProductData from './productData.js';
 import { getParams } from './utils.js';
 import ProductDetails from './productDetails.js';
+import { loadHeaderFooter } from './utils.js';
+
+loadHeaderFooter();
+
 const productId = getParams('product');
 const dataSource = new ProductData('tents');
 console.log(dataSource.getData());
