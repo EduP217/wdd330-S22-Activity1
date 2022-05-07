d3e8fcd79a9f17c944e2f2013dfe4aa85e78e470
EduP217 <prieto_217@hotmail.com>
2022-05-06 21:53:51 -0500

    stretch challenge individual activity 4

---
+++ src/js/shoppingCart.js
@@ -0,0 +1,26 @@
+import { renderListWithTemplate, getLocalStorage } from './utils.js';
+
+export default class ShoppingCart {
+  constructor(listElement) {
+    this.listElement = listElement;
+  }
+  async init() {
+    const list = getLocalStorage('so-cart');
+    this.renderList(list);
+  }
+  prepareTemplate(template, product) {
+    template.querySelector('a').href += product.Id;
+    template.querySelector('img').src = product.Images.PrimaryMedium;
+    template.querySelector('img').alt += product.Name;
+    template.querySelector('h2.card__name').textContent = product.Name;
+    template.querySelector('p.cart-card__color').textContent = product.Colors[0].ColorName;
+    //template.querySelector('p.cart-card__quantity').textContent += product.FinalPrice;
+    template.querySelector('p.cart-card__price').textContent += product.FinalPrice;
+    return template;
+  }
+  renderList(list) {
+    this.listElement.innerHTML = '';
+    const template = document.getElementById('product-cart-card-template');
+    renderListWithTemplate(template, this.listElement, list, this.prepareTemplate);
+  }
+}
