a2e58f96ed479b148cbf1d2f1c40f2375574a490
EduP217 <prieto_217@hotmail.com>
2022-05-17 08:48:55 -0500

    initial commit for product search bar

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -4,10 +4,18 @@ import { getParams, loadHeaderFooter } from './utils.js';
 
 loadHeaderFooter();
 
-const category = getParams('category');
-
-document.getElementById('product-category').innerHTML = category;
 const data = new ExternalServices();
 const listElement = document.querySelector('.product-list');
-const list = new ProductList(category, data, listElement);
+
+const category = getParams('category');
+const search = getParams('search');
+let productListType = '';
+if(category){
+    productListType = `${category} | category`;
+}
+if(search){
+    productListType = `${search} | search`;
+}
+document.getElementById('product-category').innerHTML = productListType;
+const list = new ProductList(category, search, data, listElement);
 list.init();
